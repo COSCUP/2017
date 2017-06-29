975d70b8233bfe5ee3794221c8ce1a3f880285e6
Tocknicsu <wingemerald@gmail.com>
2017-06-29 15:48:47 +0800

    (fix) navbar on windows

--- src/js/components/staff/Home.jsx
+++ src/js/components/staff/Home.jsx
@@ -1,19 +1,116 @@
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
+import Containers from 'js/containers'
 
 export default CSSModules(class extends Component {
     static propTypes = {
     }
     render () {
-        const language = this.props.language
+        const { Language } = this.props
         return (
-            <div className="subPage">
-                <div className="subpage subpage--title">
-                    <div className="subpage subpage--title">
-                        <div className="title--text">{ this.props.translate['staff'][language] }</div>
+            <div
+                style={{
+                    width: '100%'
+                }}>
+                <div className="subPage">
+                    <div className="mobile subpage--title">
+                        <div className="title--text">{ this.props.Translate['staff'][Language] }</div>
                     </div>
+                    <div className="desktop subpage--title">
+                        <div className="title--text">
+                            <div> { this.props.Translate['staff']['zh'] } </div>
+                            <div className="divider" />
+                            <div> { this.props.Translate['staff']['en'] } </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='stafflist'>
+                    <div className='card'>
+                        <div className='title'>總召</div>
+                        <div className='content'>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='title'>開車</div>
+                        <div className='content'>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='title'>開車</div>
+                        <div className='content'>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                            <div className='person'>
+                                <div className='person--wrapper'>
+                                    <img src='https://assets-cdn.github.com/favicon.ico' className='photo' />
+                                    <div className='name'>小畢</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <Containers.general.SponsorList />
                 </div>
             </div>
         )
     }
-}, require('css/sponsor/Home.styl'))
+}, require('css/staff/Home.styl'))

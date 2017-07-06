850a3dc9371ebad283ac5001d45e63d182ea1ed8
Denny Huang <denny0223@gmail.com>
2017-07-06 22:55:44 +0800

    Update Staff API endpoint

--- src/js/api/Staff.js
+++ src/js/api/Staff.js
@@ -4,7 +4,7 @@ import fetch from 'isomorphic-fetch'
 export default {
     get: async () => {
         let res = null
-        res = await fetch('https://raw.githubusercontent.com/COSCUP/coscup2017-website/master/src/json/staff.json')
+        res = await fetch('https://coscup.org/2017-assets/json/staff.json')
         res = await res.json()
         return res
     }

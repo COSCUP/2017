be291e14929723d8406ed848ef272a00ec66eedb
Jim <vic20087cjimlin@gmail.com>
2017-07-09 21:03:11 +0800

    fix footer state

--- src/js/components/general/Footer.jsx
+++ src/js/components/general/Footer.jsx
@@ -7,6 +7,31 @@ import Containers from 'js/containers'
 
 @Radium
 export default CSSModules(class extends Component {
+    constructor (props) {
+        super(props)
+        this.blurSocialHandler = this.blurSocialHandler.bind(this)
+        this.blurHistoryHandler = this.blurHistoryHandler.bind(this)
+        this.state = {
+            isBlurHistory: false,
+            isBlurSocial: false
+        }
+    }
+    blurSocialHandler () {
+        let _isBlurSocial = this.state.isBlurSocial
+        this.setState({
+            isBlurSocial: !_isBlurSocial
+        })
+        let target = document.querySelector('body')
+        target.style.overflow = (target.style.overflow === 'hidden' ? 'visible' : 'hidden')
+    }
+    blurHistoryHandler () {
+        let _isBlurHistory = this.state.isBlurHistory
+        this.setState({
+            isBlurHistory: !_isBlurHistory
+        })
+        let target = document.querySelector('body')
+        target.style.overflow = (target.style.overflow === 'hidden' ? 'visible' : 'hidden')
+    }
     async componentDidMount () {
         await this.props.getSocial()
     }
